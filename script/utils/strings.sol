1ccddfdf604cbfd70ea2433a6f926abe40c6f85a
0xAtum <0xAtum@protonmail.com>
2023-02-20 16:56:55 -0500

    Fixed warning about strings.sol

--- script/utils/strings.sol
+++ script/utils/strings.sol
@@ -42,9 +42,9 @@ library strings {
     uint256 _ptr;
   }
 
-  function memcpy(uint256 dest, uint256 src, uint256 len) private pure {
+  function memcpy(uint256 dest, uint256 src, uint256 _len) private pure {
     // Copy word-length chunks while possible
-    for (; len >= 32; len -= 32) {
+    for (; _len >= 32; _len -= 32) {
       assembly {
         mstore(dest, mload(src))
       }
@@ -54,8 +54,8 @@ library strings {
 
     // Copy remaining bytes
     uint256 mask = type(uint256).max;
-    if (len > 0) {
-      mask = 256 ** (32 - len) - 1;
+    if (_len > 0) {
+      mask = 256 ** (32 - _len) - 1;
     }
     assembly {
       let srcpart := and(mload(src), not(mask))
